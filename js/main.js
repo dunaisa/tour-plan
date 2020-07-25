ebe8522c6c979138dc9371f622678b2c8b30781b
dunaisa <elizabeths97@mail.ru>
2020-07-25 21:29:11 +0300

    create: added map in the hotel section

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ var mySwiper = new Swiper(".swiper-container", {
   effect: "coverflow",
   speed: 900,
 });
+
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var map = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [40.980109, 28.901357],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 13,
+    controls: ["zoomControl"],
+    behaviors: ["drag"],
+  });
+
+  var placemark = new ymaps.Placemark([40.980109, 28.901357], {
+    hintContent: "GRAND HILTON HOTEL",
+  });
+
+  map.geoObjects.add(placemark);
+}
