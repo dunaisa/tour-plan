eabac181b244dcb7f415a9d724b09c2286fbf1d4
dunaisa <elizabeths97@mail.ru>
2020-07-26 23:22:41 +0300

    create google maps

--- js/main.js
+++ js/main.js
@@ -12,29 +12,6 @@ var mySwiper = new Swiper(".swiper-container", {
   speed: 900,
 });
 
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var map = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [40.980109, 28.901357],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 13,
-    controls: ["zoomControl"],
-    behaviors: ["drag"],
-  });
-
-  var placemark = new ymaps.Placemark([40.980109, 28.901357], {
-    hintContent: "GRAND HILTON HOTEL",
-  });
-
-  map.geoObjects.add(placemark);
-}
-
 $(".newsletter").parallax({
   imageSrc: "img/newsletter-bg.jpg",
 });
